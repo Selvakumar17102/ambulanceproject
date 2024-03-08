ef0e701d2da59664c85e4efb238e6e4e4216ed45
selvakumar17012 <selvakumard.gts@gmail.com>
2024-03-08 11:23:57 +0530

    pull

--- api/user/donor_register_form.php
+++ api/user/donor_register_form.php
@@ -18,7 +18,7 @@
             
                 
 			    if(!empty($data->blood_donor_name) && !empty($data->blood_group)){
-                    $blood_donor_status = $data->blood_donor_status;
+                    // $blood_donor_status = $data->blood_donor_status;
                     $blood_donor_name = $data->blood_donor_name;
                     $blood_donor_age = $data->blood_donor_age;
                     $blood_donor_dob = $data->blood_donor_dob;
@@ -39,13 +39,16 @@
                     $allergies_command = $data->allergies_command;
                     $any_medication_status = $data->any_medication_status;
                     $medication_command = $data->medication_command;
+                    $bleeding_status = $data->bleeding_status;
+                    $cardiac_status = $data->cardiac_status;
+                    $hiv_status = $data->hiv_status;
                     $in_bitween_days = $data->in_bitween_days;
                     
                     $checkSql = "SELECT * FROM blood_donation WHERE user_id='$user_id'";
                     $checkResult = $conn->query($checkSql);
                     if($checkResult->num_rows == NULL){
 
-                        $sql = "INSERT INTO blood_donation (user_id,blood_donor_status,blood_donor_name,blood_donor_age,blood_donor_dob,blood_donor_gender,donor_alter_phone_no,donor_city_id,donor_address,donor_latitude,donor_longitude,blood_group,donor_height,donor_weight,ever_donate_blood_before,last_time_donated_date,any_diseases_status,diseases_command,any_allergies_status,allergies_command,take_any_medication,medication_command,in_bitween_days) VALUES ('$user_id','$blood_donor_status','$blood_donor_name','$blood_donor_age','$blood_donor_dob','$blood_donor_gender','$donor_phone_no','$donor_city_id','$donor_address','$donor_latitude','$donor_longitude','$blood_group','$donor_height','$donor_weight','$ever_donate_blood_before','$last_time_donated_date','$any_diseases_status','$diseases_command','$any_allergies_status','$allergies_command','$any_medication_status','$medication_command','$in_bitween_days')";
+                        $sql = "INSERT INTO blood_donation (user_id,blood_donor_name,blood_donor_age,blood_donor_dob,blood_donor_gender,donor_alter_phone_no,donor_city_id,donor_address,donor_latitude,donor_longitude,blood_group,donor_height,donor_weight,ever_donate_blood_before,last_time_donated_date,any_diseases_status,diseases_command,any_allergies_status,allergies_command,take_any_medication,medication_command,in_bitween_days,bleeding_status,cardiac_status,hiv_status) VALUES ('$user_id','$blood_donor_name','$blood_donor_age','$blood_donor_dob','$blood_donor_gender','$donor_phone_no','$donor_city_id','$donor_address','$donor_latitude','$donor_longitude','$blood_group','$donor_height','$donor_weight','$ever_donate_blood_before','$last_time_donated_date','$any_diseases_status','$diseases_command','$any_allergies_status','$allergies_command','$any_medication_status','$medication_command','$in_bitween_days','$bleeding_status','$cardiac_status','$hiv_status')";
                         if($conn->query($sql) === TRUE){
                             http_response_code(200);
                             $output_array['status'] = true;
--- api/user/request_register_form.php
+++ api/user/request_register_form.php
@@ -15,21 +15,25 @@
 
 			header('authorization: ' . $responceToken);
 
-                
 			    if(!empty($data->patient_name) && !empty($data->blood_group) && !empty($data->unit)){
 					$patient_name = $data->patient_name;
+					$age = $data->age;
+                    $phone_no = $data->phone_no;
+                    $alter_phone_no = $data->alter_phone_no;
                     $blood_group = $data->blood_group;
+					$blood_type = $data->blood_type;
                     $request_date = $data->request_date;
                     $unit = $data->unit;
-                    $city_id = $data->city_id;
-                    $alter_phone_no = $data->alter_phone_no;
-                    $location = $data->location;
+                    $address = $data->address;
                     $emergency_status = $data->emergency_status;
-                    $additional_notes = $data->additional_notes;
-                    $check_terms = $data->check_terms;
+					$latitude = $data->latitude;
+					$longitude = $data->longitude;
+                    // $city_id = $data->city_id;
+                    // $additional_notes = $data->additional_notes;
+                    // $check_terms = $data->check_terms;
                     $dummy_otp = rand(0001,9999);
 
-                    $sql = "INSERT INTO blood_request (user_id,patient_name,blood_group,request_date,unit,request_city_id,alter_phone_no,hospital_location,emergency_status,additional_notes,check_terms,dummy_otp) VALUES ('$user_id','$patient_name','$blood_group','$request_date','$unit','$city_id','$alter_phone_no','$location','$emergency_status','$additional_notes','$check_terms','$dummy_otp')";
+                    $sql = "INSERT INTO blood_request (user_id,patient_name,age,phone_no,alter_phone_no,blood_group,blood_type,request_date,unit,hospital_location,latitude,longitude,emergency_status,dummy_otp)VALUES ('$user_id','$patient_name','$age','$phone_no','$alter_phone_no','$blood_group','$blood_type','$request_date','$unit','$address','$latitude','$longitude','$emergency_status','$dummy_otp')";
                     if($conn->query($sql) === TRUE){
                         http_response_code(200);
                         $output_array['status'] = true;
